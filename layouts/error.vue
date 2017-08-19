bf0cd42a231d6f51b2ae908107f2e32daedd6e61
Fabio Nolasco <gomidefabio@gmail.com>
2017-08-19 11:42:52 -0400

    [fn] Added 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <h1>Error 404</h1>
+    <fn-jumbotron>
+      <h3 class="fn-text-shadow">
+        Ooops, page not found!
+      </h3>
+    </fn-jumbotron>
+
+    <div class="fn-content text-center fn-error">
+        <img src="/page404small.png" alt="Page Not Found" /><br><br>
+        Sorry, but the address that you are looking for does not exist!<br><br>
+        The system just sent me an alert, and I will investigate it as soon as possible.<br><br>
+        Meanwhile, please try using the top bar search.<br><br>
+        Thank you for your visit!
+    </div>
   </div>
 </template>
 
-<script>
-
-export default {
+<style>
+.fn-error {
+  background-color: #fff;
 }
-</script>
+</style>
